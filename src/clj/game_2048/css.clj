e7199cc567aa0b5c4acc59e73ef0932ed781a5a0
Coen Stange <coenstange@me.com>
2018-09-03 17:59:28 +0200

    + misuse REM to make site "responsive"

--- src/clj/game_2048/css.clj
+++ src/clj/game_2048/css.clj
@@ -1,31 +1,36 @@
 (ns game-2048.css
-  (:require [garden.def :refer [defstyles]]))
+  (:require [garden.def :refer [defstyles]]
+            [garden.stylesheet :refer [at-media]]))
 
 (defstyles screen
+  (at-media {:max-width "768px"}
+            [:html {:font-size "0.75em"}]) ; REM is based on the font-size, scaling down the game on a small screen
+
+  (at-media {:max-width "540px"}
+            [:html {:font-size "0.5em"}])
+
   [:body {:background-color "#ABA8AA"
           :font-family "\"Lucida Console\", Monaco, monospace"
           :color "#3A3A47"}]
 
   [:#mainContainer {:background-color "#EDEFE5"
-                    :width "50rem"
+                    :width "40rem"
                     :margin "2rem auto"
                     :border-radius "1rem"
-                    :padding "0.5rem"}]
+                    :padding "1rem"}]
 
   [:#header [:a {:float "right"}]]
 
   [:#game-panel {:background-color "#9DB6BB"
-                 :margin "1rem"
-                 :border-radius "1rem"
-                 :padding "0.5rem"}]
+                 :border-radius "1rem"}]
 
   [:.score {:background-color "#727E8C"
-            :margin "1rem"
             :border-radius "1rem"
             :padding "0.5rem"
+            :margin "1rem"
             :color "#FFFFFF"
             :text-align "center"
-            :width "20rem"
+            :width "17rem"
             :float "left"}]
 
   [:.clear {:clear "both"}])
