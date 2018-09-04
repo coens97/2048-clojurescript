4bd3ede792b5dbf9c1bdf9032732df17bb3edd71
Coen Stange <coenstange@me.com>
2018-09-04 16:21:29 +0200

    + create getter and setter for board values

--- src/cljs/game_2048/boardevents.cljs
+++ src/cljs/game_2048/boardevents.cljs
@@ -4,15 +4,27 @@
    [game-2048.db :as db]
    [day8.re-frame.tracing :refer-macros [fn-traced defn-traced]]))
 
+;; Get board value
+(defn get-board
+  [board x y]
+  (-> board
+      (nth y)
+      (nth x)))
+
+;; Set board value
+(defn set-board
+  [board x y value]
+  (assoc board y
+         (assoc (nth board y) x value)))
+
 ;; Add cell 2 to random position of the board
 (defn randomcell
   [board]
-  (let [randvalue (rand-int 4)]
-    (assoc board randvalue
-           (assoc (nth board randvalue) (rand-int 4) 2))))
+  (set-board board (rand-int 4) (rand-int 4) 2))
 
 ;; Generate empty board used at the start of the game
-(defn empty-board []
+(defn empty-board
+  []
   (->
    (into [] (map
              (fn [x] (into [] (map
@@ -22,9 +34,21 @@
    (randomcell)
    (randomcell)))
 
+(defn board-move-down
+  [board]
+  (for [y (range 2 -1 -1)] ;; (2 1 0)
+    (for [x (range 0 4)] ;; (2 1 0)
+      (let [item (->
+                  board
+                  (nth y)
+                  (nth x))]
+        (cljs.pprint/pprint item))))
+  board)
+
+(board-move-down [[0 2 2 0] [0 0 0 0] [0 0 2 0] [0 0 0 0]])
 ;; Event when user wants to move down
 (re-frame/reg-event-db
  ::movedown
  (fn-traced [db [_ _]]
-            (cljs.pprint/pprint db)
-            db))
+            (let [{board :board} db]
+              (assoc db :board (board-move-down board)))))
